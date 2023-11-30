d1eeae56ccadd578980988005d577a636c12904a
Mikhail Turilov <turilovm@gmail.com>
2023-11-30 08:02:35 +0300

    fixed_actual 2.file HW-2

--- 2_files.py
+++ 2_files.py
@@ -17,11 +17,7 @@ def main():
     В ней надо заменить pass на ваш код
     """
     with open('referat.txt', 'r', encoding='utf-8') as file:
-        my_lst = []
-        my_file = file.readlines()
-        for txt in my_file:
-            my_lst.append(txt.strip())
-        my_read = ' '.join(my_lst)
+        my_read = file.read().replace('\n', ' ')
         len_str = 0
         for str_ in my_read:
             len_str += len(str_)
